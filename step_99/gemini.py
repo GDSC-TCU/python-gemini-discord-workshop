2ba2390458f05d7e2be4bb0d2d00e9b168661bfe
Yuki <32705755+Yu-HaruWolf@users.noreply.github.com>
2024-06-27 16:52:38 +0000

    Add the get_word_response function

--- step_99/gemini.py
+++ step_99/gemini.py
@@ -5,7 +5,12 @@ class Gemini:
         genai.configure(api_key=token)
         print([model.name for model in genai.list_models()])
         self.model = genai.GenerativeModel('gemini-pro')
+        self.word_model = genai.GenerativeModel('gemini-1.5-pro', system_instruction='You are a knowledgeable teacher, please tell the meaning of the words I said in Japanese. Also, please provide example sentences in English.')
 
     def get_response(self, message):
         response = self.model.generate_content(message)
+        return response.text
+    
+    def get_word_response(self, message):
+        response = self.word_model.generate_content(message)
         return response.text
